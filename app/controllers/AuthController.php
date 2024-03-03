82c741ffeb7e2a0491517a3b66f7a80745f99b58
Michael Vogel <michael.vogel@ceff.ch>
2024-03-03 10:52:48 +0100

    Initial commit for solution

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-<<<<<<< HEAD
 class AuthController extends AppController
-=======
-class AuthController extends \App\Controllers\AppController
->>>>>>> 975bef1dd16e5596775d7a3dcd24ad3a9bc1bd36
 {
     public function index(): void
     {
