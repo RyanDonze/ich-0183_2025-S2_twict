2bb49804d3ba389ca4bb06374c4c2ba0f4013169
Michael Vogel <michael.vogel@ceff.ch>
2024-03-03 11:05:04 +0100

    Initial commit for solution

--- app/models/User.php
+++ app/models/User.php
@@ -89,7 +89,23 @@ class User extends Model
         return $success;
     }
 
-    public static function findByMailAddressAndPassword(string $mailAddress, string $password): ?array
+    public static function findByMailAddress(string $mailAddress): array
+    {
+        $db = static::getDB();
+
+        $model = $db
+            ->query(<<< SQL
+                SELECT `id`, `firstname`, `lastname`, `mailAddress`, `password`, `createdAt`, `updatedAt`
+                FROM `users`
+                WHERE `mailAddress`= '{$mailAddress}'
+                LIMIT 1;
+                SQL)
+            ->fetch();
+
+        return $model;
+    }
+
+    public static function findByMailAddressAndPassword(string $mailAddress, string $password): array
     {
         $db = static::getDB();
 
