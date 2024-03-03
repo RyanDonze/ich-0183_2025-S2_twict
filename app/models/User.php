e5d03c15af3c7cf542aae86385eee086fdfcd1b6
Michael Vogel <michael.vogel@ceff.ch>
2024-03-03 10:52:48 +0100

    Initial commit for solution

--- app/models/User.php
+++ app/models/User.php
@@ -35,13 +35,6 @@ class User extends Model
             SQL)
             ->fetch() ?: null;
 
-<<<<<<< HEAD
-=======
-        if ($model === false) {
-            $model = null;
-        }
-
->>>>>>> 750c399 (Fix model error when no result found)
         return $model;
     }
 
@@ -125,17 +118,7 @@ class User extends Model
                 LIMIT 1;
                 SQL)
             ->fetch() ?: null;
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-=======
-        if ($model === false) {
-            $model = null;
-        }
-=======
->>>>>>> fe3cfff (Fix model error when no result found)
-
->>>>>>> 750c399 (Fix model error when no result found)
         return $model;
     }
 }
