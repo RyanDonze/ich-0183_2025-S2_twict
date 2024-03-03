d236f500f7df2db92b67fb0594287fac62a69b1c
Michael Vogel <michael.vogel@ceff.ch>
2024-03-03 11:05:04 +0100

    Initial commit for solution

--- app/models/User.php
+++ app/models/User.php
@@ -92,7 +92,27 @@ class User extends Model
         return $success;
     }
 
+<<<<<<< HEAD
     public static function findByMailAddress(string $mailAddress): ?array
+=======
+    public static function findByMailAddress(string $mailAddress): array
+    {
+        $db = static::getDB();
+
+        $model = $db
+            ->query(<<< SQL
+                SELECT `id`, `firstname`, `lastname`, `mailAddress`, `password`, `createdAt`, `updatedAt`
+                FROM `users`
+                WHERE `mailAddress`= '{$mailAddress}'
+                LIMIT 1;
+                SQL)
+            ->fetch();
+
+        return $model;
+    }
+
+    public static function findByMailAddressAndPassword(string $mailAddress, string $password): array
+>>>>>>> c3c93b0 (Initial commit for solution)
     {
         $db = static::getDB();
 
