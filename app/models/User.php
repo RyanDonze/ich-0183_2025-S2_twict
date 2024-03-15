441da5bf6ee99a49c481faea132042452ca9356a
Michael Vogel <michael.vogel@ceff.ch>
2024-03-15 14:02:03 +0100

    Fix model error when no result found

--- app/models/User.php
+++ app/models/User.php
@@ -34,7 +34,17 @@ class User extends Model
                 LIMIT 1;
             SQL)
             ->fetch() ?: null;
+<<<<<<< HEAD
 
+<<<<<<< HEAD
+=======
+        if ($model === false) {
+            $model = null;
+        }
+=======
+>>>>>>> 4d669f5 (Fix model error when no result found)
+
+>>>>>>> 750c399 (Fix model error when no result found)
         return $model;
     }
 
@@ -118,7 +128,17 @@ class User extends Model
                 LIMIT 1;
                 SQL)
             ->fetch() ?: null;
+<<<<<<< HEAD
 
+<<<<<<< HEAD
+=======
+        if ($model === false) {
+            $model = null;
+        }
+=======
+>>>>>>> 4d669f5 (Fix model error when no result found)
+
+>>>>>>> 750c399 (Fix model error when no result found)
         return $model;
     }
 }
