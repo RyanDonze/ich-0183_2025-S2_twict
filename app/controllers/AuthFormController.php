a7c49b29671b1aa78b0df593c0116ece97da7577
Michael Vogel <michael.vogel@ceff.ch>
2024-03-15 14:02:03 +0100

    Fix model error when no result found

--- app/controllers/AuthFormController.php
+++ app/controllers/AuthFormController.php
@@ -6,7 +6,7 @@ namespace App\Controllers;
 
 use \App\Models\User;
 
-class AuthFormController extends \App\Controllers\AppController
+class AuthFormController extends AppController
 {
     public function login(): void
     {
